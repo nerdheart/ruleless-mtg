ea3752dba93faa221837a5aba7aaefffa15d47e2
Mouch- <michi.miedler@gmx.at>
2013-11-16 20:36:54 +0100

    +Artimes +LibGdx +Main+ stuff

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,32 @@
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Screen;
 
-public class Main {
+
+public class Main extends Game {
+	
+	@Override
+	public void create() {
+		
+		
+	}
+	
+	
+	@Override
+	public void render() {
+		
+		super.render();
+	}
+	
+	@Override
+	public void setScreen(Screen screen) {
+		
+		super.setScreen(screen);
+	}
+	
+	@Override
+	public void dispose() {
+		
+		super.dispose();
+	}
 
 }
